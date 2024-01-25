e63c18596a6b6019d0a728b72cdb52ca2c9ed3ea
larakoseff <larakoseff@gmail.com>
2024-01-25 12:13:59 +0200

    Edit contact menu

--- script.js
+++ script.js
@@ -19,23 +19,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-const btn = document.getElementById('btn')
-btn.addEventListener('click', function (e) {
-  e.preventDefault()
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const subject = document.getElementById('subject').value;
-  const message = document.getElementById('message').value;
-  const body = 'name: ' + name + '<br/> email:  ' + email + '<br/> subject: ' + subject + '<br/> message:  ' + message;
+// const btn = document.getElementById('btn')
+// btn.addEventListener('click', function (e) {
+//   e.preventDefault()
+//   const name = document.getElementById('name').value;
+//   const email = document.getElementById('email').value;
+//   const subject = document.getElementById('subject').value;
+//   const message = document.getElementById('message').value;
+//   const body = 'name: ' + name + '<br/> email:  ' + email + '<br/> subject: ' + subject + '<br/> message:  ' + message;
 
-    Email.send({
-      SecureToken: "9411fb2e-ac60-46ad-9d0f-8b1155e0854b",
-      To: 'mardouthegnu@gmail',
-      From: "larakoseff@gmail.com",
-      Subject: "contact message",
-      Body: body
-    }).then(
-      message => alert("Thank you for your message, I'll get back to you shortly.")
-    );
-})
+//     Email.send({
+//       SecureToken: "9411fb2e-ac60-46ad-9d0f-8b1155e0854b",
+//       To: 'mardouthegnu@gmail',
+//       From: "larakoseff@gmail.com",
+//       Subject: "contact message",
+//       Body: body
+//     }).then(
+//       message => alert("Thank you for your message, I'll get back to you shortly.")
+//     );
+// })
 
