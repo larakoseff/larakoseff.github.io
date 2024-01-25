fd10d81ed987c11fa159166678b71a067f1b4154
larakoseff <larakoseff@gmail.com>
2024-01-25 10:25:34 +0200

    Move more JS to script.js

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+// Contact form 
+
+const btn = document.getElementById('btn')
+btn.addEventListener('click', function (e) {
+  e.preventDefault()
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const subject = document.getElementById('subject').value;
+  const message = document.getElementById('message').value;
+  const body = 'name: ' + name + '<br/> email:  ' + email + '<br/> subject: ' + subject + '<br/> message:  ' + message;
+
+    Email.send({
+      SecureToken: "9411fb2e-ac60-46ad-9d0f-8b1155e0854b",
+      To: 'mardouthegnu@gmail',
+      From: "larakoseff@gmail.com",
+      Subject: "contact message",
+      Body: body
+    }).then(
+      message => alert("Thank you for your message, I'll get back to you shortly.")
+    );
+})
+
